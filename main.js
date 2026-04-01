8f4a379af7467ab53ccc4e17953a315bb8480c11
Kaito.Tamura <tamura.kaito2025@gmail.com>
2026-04-01 20:17:46 +0900

    ToDo削除機能を追加

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const todoForm = document.querySelector('#todo-form');
+const todoInput = document.querySelector('#todo-input');
+const todoList = document.querySelector('#todo-list');
+
+todoForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const task = todoInput.value;
+
+  if (!task) return;
+
+  const li = document.createElement('li');
+
+  li.textContent = task;
+
+  const deleteBtn = document.createElement('button');
+
+  deleteBtn.textContent = '削除';
+
+  deleteBtn.addEventListener('click', () => {
+    li.remove();
+  });
+
+  li.appendChild(deleteBtn);
+
+  todoList.appendChild(li);
+});
